83564af4923eb478b9550817314a5e4652f922c4
Filipe Gabriel <filipegabriel11@hotmail.com>
2024-08-25 19:29:13 -0300

    Content created

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+    id: number = 1
     photoCover: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkPe9pNfrShEJZd-9vbLg3pzW7wQuea_pLxg&s"
     cardTitle: string = "Novo homem de ferro anunciado"
     cardDescription: string = "Marvel anuncia novo fime do homem de ferro"
